8ae2a4a049d0d2127182f950e6c6280b55d60cf0
Darya_Egorova <62828766+DaryaEgorova@users.noreply.github.com>
2021-06-11 03:29:43 +0300

    ВОТ ЭТО ДАШИН ВАРИАНТ

---
+++ Controllers/HomeController1.cs
@@ -0,0 +1,58 @@
+﻿//using Microsoft.AspNetCore.Mvc;
+//using Microsoft.Extensions.Logging;
+//using System;
+//using System.Collections.Generic;
+//using System.Diagnostics;
+//using System.Linq;
+//using System.Threading.Tasks;
+//using Users.Models;
+
+//namespace Users.Controllers
+//{
+//    public class HomeController : Controller
+//    {
+//        private readonly ILogger<HomeController> _logger;
+
+//        public HomeController(ILogger<HomeController> logger)
+//        {
+//            _logger = logger;
+//        }
+
+//        public IActionResult Index()
+//        {
+//            return View();
+//        }
+
+//        public IActionResult Privacy()
+//        {
+//            return View();
+//        }
+
+//        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+//        public IActionResult Error()
+//        {
+//            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+//        }
+//    }
+//}
+
+
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthUsers.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            Dictionary<string, object> data
+                = new Dictionary<string, object>();
+            data.Add("Ключ", "Значение");
+
+            return View(data);
+        }
+    }
+}
