739a7f51f5aa3003e0873b84a183d6d47c445710
Darya_Egorova <62828766+DaryaEgorova@users.noreply.github.com>
2021-06-11 03:40:03 +0300

    Identity (с ошибками)

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Users.Data;
+
+[assembly: HostingStartup(typeof(Users.Areas.Identity.IdentityHostingStartup))]
+namespace Users.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            
+        }
+        public void Configure(IWebHostBuilder builder, IApplicationBuilder app)
+        {
+            //builder.ConfigureServices((context, services) => {
+            //    services.AddDbContext<UsersContext>(options =>
+            //        options.UseSqlServer(
+            //            context.Configuration.GetConnectionString("UsersContextConnection")));
+
+            //    services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            //        .AddEntityFrameworkStores<UsersContext>();
+            //});
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+        }
+
+       
+    }
+}
